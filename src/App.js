e5fcf5945cfb0ccf943bbc375209c0aa18d331cd
inakiesk <inakieskruiz@gmail.com>
2023-04-01 18:47:29 -0600

    Update App.js with new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './pages/components/Navbar';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
