7ef8b8d0ee3fb534d81bfec42009ccd3ed3dd72f
inakiesk <inakieskruiz@gmail.com>
2023-04-14 00:55:57 -0600

    Add booksSlice.js component

---
+++ src/pages/redux/books/booksSlice.js
@@ -0,0 +1,28 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+
+const initialState = {
+  books: [],
+}
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => ({
+      books: [
+        state.books,
+        {
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ],
+    }),
+    removeBook: (state, action) => ({
+      ...state,
+      books: state.books.filter((book) => book.title !== action.payload.title),
+    }),
+  },
+});
+
+module.exports = bookSlice.reducer;
+export default bookSlice.actions;
