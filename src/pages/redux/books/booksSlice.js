4c88ecf295882bdc9c9dfce3378f365f067016bc
inakiesk <inakieskruiz@gmail.com>
2023-04-15 03:10:17 -0600

    Update state of bookSlice

--- src/pages/redux/books/booksSlice.js
+++ src/pages/redux/books/booksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      itemId: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
@@ -12,14 +31,16 @@ const bookSlice = createSlice({
       books: [
         state.books,
         {
+          itemId: Math.floor(Math.random() * 100),
           title: action.payload.title,
           author: action.payload.author,
+          category: action.payload.category,
         },
       ],
     }),
     removeBook: (state, action) => ({
       ...state,
-      books: state.books.filter((book) => book.title !== action.payload.title),
+      books: state.books.filter((book) => book.itemId !== action.payload.itemId),
     }),
   },
 });
